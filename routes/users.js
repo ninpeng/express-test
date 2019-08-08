46feb638eba0fcb1dd2fb996d48e2ea0fb340d6f
bcchoi <bcchoi@metaps-plus.com>
2019-08-08 14:36:47 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* GET users listing. */
+router.get('/getuser/:username', function(req, res, next) {
+  res.send(req.params.username||'error');
+});
+
 module.exports = router;
